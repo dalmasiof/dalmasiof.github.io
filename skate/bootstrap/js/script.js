029995b66875ffc3d67309cc254fbce778fa1a8b
dalmasiof <39097961+dalmasiof@users.noreply.github.com>
2018-08-09 13:12:58 -0300

    att

---
+++ skate/bootstrap/js/script.js
@@ -0,0 +1,76 @@
+debounce = function(func, wait, immediate) {
+          var timeout;
+          return function() {
+            var context = this, args = arguments;
+            var later = function() {
+              timeout = null;
+              if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+          };
+        };
+
+                (function(){
+            var $target = $('.anima'),
+                animationClass = 'anima-start',
+                offset = $(window).height() * 3/4;
+
+            function animeScroll() {
+              var documentTop = $(document).scrollTop();
+
+              $target.each(function(){
+                var itemTop = $(this).offset().top;
+                if (documentTop > itemTop - offset) {
+                  $(this).addClass(animationClass);
+                } else {
+                  $(this).removeClass(animationClass);
+                }
+              });
+            }
+
+            animeScroll();
+
+            $(document).scroll(debounce(function(){
+              animeScroll();
+            }, 200));
+          })();
+
+           (function(){
+            var $target = $('.anima-inverso'),
+                animationClass = 'anima-start',
+                offset = $(window).height() * 3/4;
+
+            function animeScroll() {
+              var documentTop = $(document).scrollTop();
+
+              $target.each(function(){
+                var itemTop = $(this).offset().top;
+                if (documentTop > itemTop - offset) {
+                  $(this).addClass(animationClass);
+                } else {
+                  $(this).removeClass(animationClass);
+                }
+              });
+            }
+
+            animeScroll();
+
+            $(document).scroll(debounce(function(){
+              animeScroll();
+            }, 200));
+          })();
+/*---------------------------------------------------------*/
+$('.nav a[href^="#"]').on('click', function(e) {
+  e.preventDefault();
+  var id = $(this).attr('href'),
+      targetOffset = $(id).offset().top;
+      
+  $('html, body').animate({ 
+    scrollTop: targetOffset - 100
+  }, 500);
+});
+
+
